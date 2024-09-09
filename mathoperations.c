335cdfc543700a138a7a79d066c6d82aad58f45d
admin <madhubellamkonda76@gmail.com>
2024-09-09 11:07:56 +0530

    adding c sample file

---
+++ mathoperations.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sum,sub,mul;
+printf("Enter the value of a\n");
+
+scanf("%d",&a);
+printf("Enter the value of b \n");
+scanf("%d",&b);
+printf("\naddition of two numbers%d\n",a+b);
+printf("\nsubstraction of two numbers%d\n",a-b);
+printf("\nmultiplication of two numbers%d\n",a+b);
+
+}
